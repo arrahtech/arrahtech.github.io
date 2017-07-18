bdaef436c306436001c4f4728ac9fd2dd36aa36f
Arun Y <arunwizz@gmail.com>
2017-07-18 23:35:09 +0530

    wip

--- arrahtech_files/zerif.js
+++ arrahtech_files/zerif.js
@@ -4,8 +4,8 @@
 $.vegas('slideshow', {
   delay:7000,
   backgrounds:[
-    { src:'arrahtech_files/b2.jpg', fade:1000 },
-    { src:'arrahtech_files/b3.jpg', fade:1000 },
+    { src:'b2.jpg', fade:1000 },
+    { src:'b3.jpg', fade:1000 },
   ]
 });
       
